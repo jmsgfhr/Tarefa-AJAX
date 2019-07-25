54827d67b009148312d42bbf7f63be4a76193510
Jonathan Meirelles <jonathanms30@gmail.com>
2019-07-25 19:48:22 -0300

    Livros carregam em um vetor, e agora são adicionados ao table

---
+++ script.js
@@ -0,0 +1,37 @@
+let newBookListItem = document.querySelector('#book-list table');
+
+const myHeaders = {
+    "Content-Type": "application/json"
+}
+
+const fetchingConfig = {
+    method: 'GET',
+    headers: myHeaders
+}
+
+let booksToLoad;
+
+fetch('https://treinamento-api.herokuapp.com/books', fetchingConfig)
+.then((request) => {
+    return request.json();
+})
+.then((responseAsJson) => {
+    booksToLoad = responseAsJson;
+}).then(() => {
+    for (let i = 0; i < booksToLoad.length; i++) {
+        let newBookLine = document.createElement('tr');
+        let newBookName = document.createElement('td');
+        let newBookAutor = document.createElement('td');
+        let newBookCreated = document.createElement('td');
+        let newBookUpdate = document.createElement('td');
+        newBookName.innerText = booksToLoad[i].name;
+        newBookAutor.innerText = booksToLoad[i].author;
+        newBookCreated.innerText = booksToLoad[i].created_at;
+        newBookUpdate.innerText = booksToLoad[i].updated_at;
+        newBookLine.appendChild(newBookName);
+        newBookLine.appendChild(newBookAutor);
+        newBookLine.appendChild(newBookCreated);
+        newBookLine.appendChild(newBookUpdate);
+        newBookListItem.appendChild(newBookLine);
+    }
+})
