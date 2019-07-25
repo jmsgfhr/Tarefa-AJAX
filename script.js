4d7c5764b681dbd08acfc3a7f30c28c386b0f7ec
Jonathan Meirelles <jonathanms30@gmail.com>
2019-07-25 19:27:19 -0300

    Livros carregam em um vetor, e agora são adicionados ao ul

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let newBookListItem = document.querySelector('#book-list ul');
+
 const myHeaders = {
     "Content-Type": "application/json"
 }
@@ -17,6 +19,15 @@ fetch('https://treinamento-api.herokuapp.com/books', fetchingConfig)
     booksToLoad = responseAsJson;
 }).then(() => {
     for (let i = 0; i < booksToLoad.length; i++) {
-        console.log(booksToLoad[i]);
+        let newBookItem = document.createElement('li');
+        let newBookButton = document.createElement('span');
+        newBookButton.classList.add('delete');
+        newBookButton.innerText = "excluir"
+        let newBookName = document.createElement('span');
+        newBookName.innerText = `Livro: ${booksToLoad[i].name} Autor: ${booksToLoad[i].author}`;
+        newBookName.classList.add('name');
+        newBookItem.appendChild(newBookButton);
+        newBookItem.appendChild(newBookName);
+        newBookListItem.appendChild(newBookItem);
     }
 })
