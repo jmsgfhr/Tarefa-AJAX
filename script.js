cd32bb0a483daafba3dea3fa747e85acf6773beb
Jonathan Meirelles <jonathanms30@gmail.com>
2019-07-25 19:11:00 -0300

    Livros carregam em um vetor, mas não são adicionados ao HTML

---
+++ script.js
@@ -0,0 +1,22 @@
+const myHeaders = {
+    "Content-Type": "application/json"
+}
+
+const fetchingConfig = {
+    method: 'GET',
+    headers: myHeaders
+}
+
+let booksToLoad;
+
+fetch('https://treinamento-api.herokuapp.com/books', fetchingConfig)
+.then((request) => {
+    return request.json();
+})
+.then((responseAsJson) => {
+    booksToLoad = responseAsJson;
+}).then((booksToLoad) => {
+    for (let i = 0; i < booksToLoad.length; i++) {
+    
+    }
+})
