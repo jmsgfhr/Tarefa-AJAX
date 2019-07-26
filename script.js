dc97d9394571be4e538645f48c5f7bc6df99f98e
Jonathan Meirelles <jonathanms30@gmail.com>
2019-07-25 21:14:12 -0300

    Feito o bonus

--- script.js
+++ script.js
@@ -24,22 +24,27 @@ fetch('https://treinamento-api.herokuapp.com/books', fetchingConfig)
     for (let i = 0; i < booksToLoad.length; i++) {
         let newBookLine = document.createElement('tr');
         let newBookName = document.createElement('td');
+        let newBookId = document.createElement('td');
         let newBookAutor = document.createElement('td');
         let newBookCreated = document.createElement('td');
         let newBookUpdate = document.createElement('td');
         let newBookButton = document.createElement('span');
         newBookButton.classList.add('delete');
         newBookButton.innerText = "excluir";
+        newBookId.innerText = booksToLoad[i].id;
         newBookName.innerText = booksToLoad[i].name;
         newBookAutor.innerText = booksToLoad[i].author;
         newBookCreated.innerText = booksToLoad[i].created_at;
         newBookUpdate.innerText = booksToLoad[i].updated_at;
+        newBookId.classList.add('id');
+        newBookLine.appendChild(newBookId);
         newBookLine.appendChild(newBookName);
         newBookLine.appendChild(newBookAutor);
         newBookLine.appendChild(newBookCreated);
         newBookLine.appendChild(newBookUpdate);
         newBookLine.appendChild(newBookButton);
         newBookListItem.appendChild(newBookLine);
+        
     }
 })
 
@@ -69,21 +74,26 @@ function Post(name,author) {
         let newBookLine = document.createElement('tr');
         let newBookName = document.createElement('td');
         let newBookAutor = document.createElement('td');
+        let newBookId = document.createElement('td');
         let newBookCreated = document.createElement('td');
         let newBookUpdate = document.createElement('td');
         let newBookButton = document.createElement('span');
         newBookButton.innerText = "excluir";
+        newBookId.innerText = responseAsJson.id;
         newBookName.innerText = responseAsJson.name;
         newBookAutor.innerText = responseAsJson.author;
         newBookCreated.innerText = responseAsJson.created_at;
         newBookUpdate.innerText = responseAsJson.updated_at;
         newBookButton.classList.add('delete');
+        newBookId.classList.add('id');
+        newBookLine.appendChild(newBookId);
         newBookLine.appendChild(newBookName);
         newBookLine.appendChild(newBookAutor);
         newBookLine.appendChild(newBookCreated);
         newBookLine.appendChild(newBookUpdate);
         newBookLine.appendChild(newBookButton);
         newBookListItem.appendChild(newBookLine);
+        
     })
 }
 
@@ -92,6 +102,20 @@ addBookButton.addEventListener('click', (e) => {
     Post(newBook[0].value,newBook[1].value);
 })
 
+function deletarLivroPeloID(id) {
+    fetch(`https://treinamento-api.herokuapp.com/books/${id}`, {
+        method: 'DELETE'
+    }).then((response) => {
+        if (response.status === 404) alert("Excluído");
+        else console.log(response);
+    }).catch((err) => {
+        alert(err)
+    })
+}
+
 deleteButton.addEventListener('click', (e) => {
-    if(e.target.nodeName==='SPAN' && e.target.className === 'delete') e.target.parentNode.remove();
+    if(e.target.nodeName==='SPAN' && e.target.className === 'delete'){
+        deletarLivroPeloID(e.target.parentNode.querySelector(".id").innerText);
+        e.target.parentNode.remove();
+    }
 });
